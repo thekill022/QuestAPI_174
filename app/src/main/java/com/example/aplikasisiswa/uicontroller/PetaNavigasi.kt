0211c43b43bd9e552807c29f142e95345b671395
thekill022 <gibfathoni@gmail.com>
2025-12-19 17:27:16 +0700

    membuat composable function HostNavigasi dan mendaftarkan home sebagai route

--- app/src/main/java/com/example/aplikasisiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/aplikasisiswa/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.aplikasisiswa.modelData.DataSiswa
+import com.example.aplikasisiswa.uicontroller.route.DestinasiHome
+import com.example.aplikasisiswa.view.HomeScreen
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(),
                  modifier : Modifier) {
     HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = {}, navigateToItemUpdate = {})
+        }
+    }
 }
