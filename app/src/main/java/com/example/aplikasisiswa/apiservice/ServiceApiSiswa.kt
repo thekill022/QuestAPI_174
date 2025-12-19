947c57a07664f76a35c5cd97fba9b1ecb59fa8e8
thekill022 <gibfathoni@gmail.com>
2025-12-19 10:23:30 +0700

    membuat method dan data class untuk detail siswa

--- app/src/main/java/com/example/aplikasisiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/aplikasisiswa/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.aplikasisiswa.apiservice
 
+import com.example.aplikasisiswa.modelData.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -8,7 +9,7 @@ interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-    @POST
+    @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
 }
