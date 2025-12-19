dbd91a45b7aac2d88949a0704496783f62d526e1
thekill022 <gibfathoni@gmail.com>
2025-12-19 17:19:22 +0700

    membuat HalamanHome.kt

---
+++ app/src/main/java/com/example/aplikasisiswa/view/HalamanHome.kt
@@ -0,0 +1,175 @@
+package com.example.aplikasisiswa.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.aplikasisiswa.R
+import com.example.aplikasisiswa.modelData.DataSiswa
+import com.example.aplikasisiswa.uicontroller.route.DestinasiHome
+import com.example.aplikasisiswa.viewmodel.HomeViewModel
+import com.example.aplikasisiswa.viewmodel.StatusUiSiswa
+import com.example.aplikasisiswa.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry : () -> Unit,
+    navigateToItemUpdate : (Int) -> Unit,
+    modifier : Modifier = Modifier,
+    viewModel : HomeViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)
+                )
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(id = R.string.entry_siswa))
+            }
+        }
+    ) { innerPadding ->
+        HomeBody(
+            statusUiSiswa = viewModel.listSiswa,
+            onSiswaClick = navigateToItemUpdate,
+            retryAction = viewModel::loadSiswa,
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun HomeBody(
+    statusUiSiswa: StatusUiSiswa,
+    onSiswaClick : (Int) -> Unit,
+    retryAction : () -> Unit,
+    modifier : Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        when(statusUiSiswa) {
+            is StatusUiSiswa.Loading -> LoadingScreen()
+            is StatusUiSiswa.Success -> DaftarSiswa(
+                itemSiswa = statusUiSiswa.siswa,
+                onSiswaClick = {onSiswaClick(it.id)}
+            )
+            is StatusUiSiswa.Error -> ErrorScreen(
+                retryAction,
+                modifier = modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(painter = painterResource(
+        id = R.drawable.loading_img),
+        contentDescription = stringResource(id = R.string.loading),
+        modifier = modifier.size(200.dp))
+}
+
+@Composable
+fun DaftarSiswa(
+    itemSiswa : List<DataSiswa>,
+    onSiswaClick : (DataSiswa) -> Unit,
+    modifier: Modifier= Modifier
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = {it.id}) {
+            person ->
+            ItemSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+                    .clickable { onSiswaClick(person) }
+            )
+        }
+    }
+}
+
+@Composable
+fun ItemSiswa(
+    siswa : DataSiswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = siswa.nama, style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(imageVector = Icons.Default.Phone, contentDescription = null)
+                Text(text = siswa.telpon, style = MaterialTheme.typography.titleMedium)
+            }
+            Text(text = siswa.alamat, style = MaterialTheme.typography.titleMedium)
+        }
+    }
+}
+
+@Composable
+fun ErrorScreen(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = R.string.gagal), modifier = Modifier.padding(16.dp))
+        Button(onClick = {retryAction}) {
+            Text(text = stringResource(id = R.string.retry))
+        }
+    }
+}
