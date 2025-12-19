0fb2a0c11c4a07f5fefac69a8189293c261bc4d3
thekill022 <gibfathoni@gmail.com>
2025-12-19 09:59:27 +0700

    membuat service untuk get dan post data siswa

---
+++ app/src/main/java/com/example/aplikasisiswa/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.aplikasisiswa.apiservice
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+}
