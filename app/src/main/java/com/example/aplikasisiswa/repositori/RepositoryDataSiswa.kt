622bb9128032e4c23c78d69352d5dab830cceaa1
thekill022 <gibfathoni@gmail.com>
2025-12-19 10:29:11 +0700

    membuat class JaringanRepositoryDataSiswa

--- app/src/main/java/com/example/aplikasisiswa/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/aplikasisiswa/repositori/RepositoryDataSiswa.kt
@@ -1,8 +1,17 @@
 package com.example.aplikasisiswa.repositori
 
+import com.example.aplikasisiswa.apiservice.ServiceApiSiswa
 import com.example.aplikasisiswa.modelData.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
