b612f27ca01db202cceaadeabd4ad988689e4187
thekill022 <gibfathoni@gmail.com>
2025-12-19 16:36:45 +0700

    membuat top bar untuk aplikasi

---
+++ app/src/main/java/com/example/aplikasisiswa/view/SiswaTopAppBar.kt
@@ -0,0 +1,37 @@
+package com.example.aplikasisiswa.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.aplikasisiswa.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title : String,
+    canNavigateBack : Boolean,
+    modifier : Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp : () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(text = title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = { navigateUp }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = stringResource(
+                        id = R.string.back
+                    ))
+                }
+            }
+        })
+}
