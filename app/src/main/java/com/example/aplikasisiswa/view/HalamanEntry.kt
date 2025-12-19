94b5a7a3f97a1b67d903e455ab66f11f3f08c69a
thekill022 <gibfathoni@gmail.com>
2025-12-19 18:25:28 +0700

    membuat HalamanEntry.kt

---
+++ app/src/main/java/com/example/aplikasisiswa/view/HalamanEntry.kt
@@ -0,0 +1,134 @@
+package com.example.aplikasisiswa.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.example.aplikasisiswa.R
+import com.example.aplikasisiswa.modelData.DetailSiswa
+import com.example.aplikasisiswa.modelData.UIStateSiswa
+import com.example.aplikasisiswa.uicontroller.route.DestinasiEntry
+import com.example.aplikasisiswa.viewmodel.EntryViewModel
+import com.example.aplikasisiswa.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack : () -> Unit,
+    modifier : Modifier = Modifier,
+    viewModel : EntryViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(title = stringResource(id = DestinasiEntry.titleRes), canNavigateBack = true, navigateUp = navigateBack, scrollBehavior = scrollBehavior)
+        }
+    ) {innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa : UIStateSiswa,
+    onSiswaValueChange : (DetailSiswa) -> Unit,
+    onSaveClick : () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormTambahSiswa(detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth())
+        Button(onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
+    }
+}
+
+@Composable
+fun FormTambahSiswa (
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+) {
+            Column (
+                modifier = modifier,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+            ){
+        OutlinedTextField(
+        value =  detailSiswa.nama,
+        onValueChange =  {onValueChange (detailSiswa.copy(nama = it)) },
+        Modifier.fillMaxWidth(),
+        label = { Text(stringResource(R.string.nama)) },
+        enabled = enabled,
+        singleLine = true)
+
+        OutlinedTextField(
+        value =  detailSiswa.alamat,
+        onValueChange =  {onValueChange (detailSiswa.copy(alamat = it)) },
+                    Modifier.fillMaxWidth(),
+        label = { Text(stringResource(R.string.alamat)) },
+        enabled = enabled,
+        singleLine = true)
+
+        OutlinedTextField(
+        value =  detailSiswa.telpon,
+        onValueChange =  {onValueChange (detailSiswa.copy(telpon = it)) },
+                    Modifier.fillMaxWidth(),
+        label = { Text(stringResource(R.string.telpon)) },
+        enabled = enabled,
+        singleLine = true)
+
+            if (enabled) {
+                Text(
+                    text = stringResource(R.string.required_field),
+                    modifier = Modifier.padding(start = dimensionResource(id = R.dimen .padding_medium))
+                )
+            }
+                    Divider(
+                    thickness = dimensionResource(R.dimen.padding_small),
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium)))
+            }
+}
