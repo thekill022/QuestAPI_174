d2ea20333d54950c6d2fedeff0a61ad4559c94c3
thekill022 <gibfathoni@gmail.com>
2025-12-26 21:13:44 +0700

    perubahan destinasi route diparameter navigateToItemUpdate di HomeScreen

--- app/src/main/java/com/example/aplikasisiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/aplikasisiswa/uicontroller/PetaNavigasi.kt
@@ -32,7 +32,7 @@ fun HostNavigasi(
 ) {
     NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
         composable(DestinasiHome.route) {
-            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)}, navigateToItemUpdate = {navController.navigate("${DestinasiEdit.route}/$it")})
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)}, navigateToItemUpdate = {navController.navigate("${DestinasiDetail.route}/$it")})
         }
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
