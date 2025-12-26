31d8eba434f9f67d46c91e3def74dc18a55d1983
thekill022 <gibfathoni@gmail.com>
2025-12-26 18:33:30 +0700

    Menambahkan method delete berdasarkan id untuk menghapus data siswa

--- app/src/main/java/com/example/aplikasisiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/aplikasisiswa/apiservice/ServiceApiSiswa.kt
@@ -2,6 +2,7 @@ package com.example.aplikasisiswa.apiservice
 
 import com.example.aplikasisiswa.modelData.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -19,5 +20,8 @@ interface ServiceApiSiswa {
 
     @PUT("editTM.php/{id}")
     suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id:Int):retrofit2.Response<Void>
 
 }
