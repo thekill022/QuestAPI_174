201403a08302ec4a0c3b0b4730ba0e1ed6d5cbfa
thekill022 <gibfathoni@gmail.com>
2025-12-26 20:07:45 +0700

    membuat DestinasiEdit

--- app/src/main/java/com/example/aplikasisiswa/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/aplikasisiswa/uicontroller/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.aplikasisiswa.uicontroller.route
 
-object DestinasiEdit {
+import com.example.aplikasisiswa.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemArg}"
 }
