a4f137a64ffadc6fb5230865927aeb9404aad358
thekill022 <gibfathoni@gmail.com>
2025-12-26 18:38:53 +0700

    menambahkan methdo method yang sudah ditambahkan ke repositori

--- app/src/main/java/com/example/aplikasisiswa/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/aplikasisiswa/repositori/RepositoryDataSiswa.kt
@@ -7,6 +7,9 @@ import retrofit2.Response
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -14,4 +17,7 @@ class JaringanRepositoryDataSiswa(
 ) : RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int) : DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
