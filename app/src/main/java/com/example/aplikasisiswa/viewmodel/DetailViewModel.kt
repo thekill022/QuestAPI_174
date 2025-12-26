72c86c8d48f60fb1f058ec7bc05f54ee41d4f4d5
thekill022 <gibfathoni@gmail.com>
2025-12-26 18:47:53 +0700

    membuat sealed interface untuk status UI di DetailViewModel.kt

--- app/src/main/java/com/example/aplikasisiswa/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/aplikasisiswa/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.aplikasisiswa.viewmodel
 
+import com.example.aplikasisiswa.modelData.DataSiswa
+
+sealed interface StatusUIDetail {
+ data class Success(val satuSiswa : DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
