a1372d7510593cd594ae462452cedff90ae2a083
thekill022 <gibfathoni@gmail.com>
2025-12-26 19:48:58 +0700

    melengkapi isi composable function DetailSiswaScreen

--- app/src/main/java/com/example/aplikasisiswa/view/HalamanDetail.kt
+++ app/src/main/java/com/example/aplikasisiswa/view/HalamanDetail.kt
@@ -1,5 +1,9 @@
 package com.example.aplikasisiswa.view
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,9 +13,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -19,6 +29,7 @@ import com.example.aplikasisiswa.viewmodel.DetailViewModel
 import com.example.aplikasisiswa.viewmodel.provider.PenyediaViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplikasisiswa.R
+import com.example.aplikasisiswa.modelData.DataSiswa
 import com.example.aplikasisiswa.uicontroller.route.DestinasiDetail
 import com.example.aplikasisiswa.viewmodel.StatusUIDetail
 import kotlinx.coroutines.launch
@@ -66,6 +77,49 @@ fun DetailSiswaScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState()))
+    }
+
+}
+
+@Composable
+fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete : () -> Unit,
+    modifier : Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable {
+            mutableStateOf(false)
+        }
+        when(statusUIDetail) {
+            is StatusUIDetail.Success -> DetailDataSiswa(siswa = statusUIDetail.satuSiswa, modifier = Modifier.fillMaxWidth())
+            else -> {}
+        }
+        OutlinedButton(
+            onClick = {
+                deleteConfirmationRequired = true
+            },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+            ) {
+            Text(text = stringResource(id = R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = {
+                    deleteConfirmationRequired = false
+                },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
     }
 
 }
